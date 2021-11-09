97c802630e8086da128cde716e001785eeb4dc6e
julio1703 <89707649+julio1703@users.noreply.github.com>
2021-11-09 12:41:28 +0200

    updated v1

---
+++ middleware/getUser.js
@@ -0,0 +1,11 @@
+// function getUser(req, res, next) {
+//   const token = req.cookies.user.token;
+//   if (token) {
+//     const user = jwt.verify(token, SECRET);
+//     req.user = user;
+//   }
+//   next();
+// }
+// module.exports = getUser;
+
+// fetch('/',{method:'post'})
--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const http = require("http");
-const port = process.env.PORT || 3000;
 const path = require("path");
 const express = require("express");
+const jwt = require("jsonwebtoken");
+// const getUser = require("./middleware/getUser.js");
 const cookieparser = require("cookie-parser");
 const dataHandler = require("./handlers/data.js");
 
+const SECRET = "nkA$SD82&&282hd";
+const PORT = process.env.PORT || 3000;
+
 const server = express();
+
 server.use(cookieparser());
 server.use(express.urlencoded());
 
-server.listen(port, () =>
-  console.log(`listening on http://localhost:${port}....`)
+// server.use(getUser);
+server.listen(PORT, () =>
+  console.log(`listening on http://localhost:${PORT}....`)
 );
 
 server.get("/", (req, res) => {
-  res.sendfile(path.join(__dirname));
+  res.sendfile(path.join(__dirname, "www", "index.html"));
 });
+
+server.get("/log-in", (req, res) => {
+  res.sendfile(path.join(__dirname, "www", "loginpage.html"));
+});
+
 server.get("/data/pokemon/:name", dataHandler);
 
+server.post("/log-in", (req, res) => {
+  const email = req.body.email;
+});
 server.use(express.static(path.join(__dirname, "www")));
